55f0d6cf96f0fd8160cd0d07d687698dea0e2afe
jiangzhh <akynazh@gmail.com>
2024-06-13 01:00:41 +0800

    feat: sonic

---
+++ build.sh
@@ -0,0 +1,4 @@
+go build -v -o bepusdt ./main
+
+# pm2 start bepusdt
+# pm2 restart bepusdt
--- main/main.go
+++ main/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Version 版本号说明 1.0.0 代表主版本号.功能版本号.修订号
-const Version = "2.0.0"
+const Version = "3.0.0"
 
 func main() {
 	if err := model.Init(); err != nil {
